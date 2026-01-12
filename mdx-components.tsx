ea8f4518c101fb93d0b7529d6a1c81c051142c9c
yasarajayadinee <yasara.jayadinee@Directfn.com>
2026-01-12 16:08:03 +0530

    Fix build & Pagefind search

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,7 +2,7 @@ import { useMDXComponents as getDocsMDXComponents } from 'nextra-theme-docs';
 
 const docsComponents = getDocsMDXComponents();
 
-export const useMDXComponents: typeof getDocsMDXComponents = (components) => ({
+export const useMDXComponents: typeof getDocsMDXComponents = (components = {}) => ({
   ...docsComponents,
   ...components,
 });
